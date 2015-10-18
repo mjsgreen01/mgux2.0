e250d27623cd96e40a1a3dfe272e06980ff21551
mjsgreen01 <mjsgreen01@gmail.com>
2015-10-18 18:25:57 -0400

    [feat] start adding projects data

--- app/data/data.js
+++ app/data/data.js
@@ -1,3 +1,14 @@
 {
-  "projects": "DATA IN DATA.JS"
+  "projects": {
+    "psal": {
+      "name": "Public Schools Athletic League",
+      "projectType": "mobile app",
+      "description": ""
+    },
+    "admissionsAssessment": {
+      "name": "Admissions Assessment",
+      "projectType": "tablet & web apps",
+      "description": ""
+    }
+  }
 }

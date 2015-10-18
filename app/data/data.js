958f900bb251ae4569417999374789f24ff721db
mjsgreen01 <mjsgreen01@gmail.com>
2015-10-18 19:42:32 -0400

    [feat] more projects data added

--- app/data/data.js
+++ app/data/data.js
@@ -1,14 +1,44 @@
 {
-  "projects": {
-    "psal": {
+  "projects": [
+    {
       "name": "Public Schools Athletic League",
+      "alias": "psal",
       "projectType": "mobile app",
-      "description": ""
+      "bgImage": "",
+      "logo": "",
+      "descriptionUrl": ""
     },
-    "admissionsAssessment": {
+    {
       "name": "Admissions Assessment",
+      "alias": "erb",
       "projectType": "tablet & web apps",
-      "description": ""
+      "bgImage": "",
+      "logo": "",
+      "descriptionUrl": ""
+    },
+    {
+      "name": "Thanksgiving Day Parade",
+      "alias": "macys",
+      "projectType": "mobile app",
+      "bgImage": "",
+      "logo": "",
+      "descriptionUrl": ""
+    },
+    {
+      "name": "Moodsnap",
+      "alias": "moodsnap",
+      "projectType": "mobile app",
+      "bgImage": "",
+      "logo": "",
+      "descriptionUrl": ""
+    },
+    {
+      "name": "Office of the Comptroller",
+      "alias": "comp",
+      "projectType": "website",
+      "bgImage": "",
+      "logo": "",
+      "descriptionUrl": ""
     }
-  }
+  ]
 }
